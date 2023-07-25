e20ff39915b5f733646f4e793f6482b08604a29e
shreyansh223 <shreyansh.gupta0440@gmail.com>
2023-07-25 21:08:33 +0530

    added cursor
    added a cursor that matches theme

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
 import 'animate.css';
 import './App.css';
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 import Landing from './components/Landing';
+import { useRef } from 'react';
+import { useState } from 'react';
 import {
-	Routes,
-	Route,
-	BrowserRouter as Router,
-	Navigate,
-} from "react-router-dom";
+  Routes,
+  Route,
+  BrowserRouter as Router,
+  Navigate,
+} from 'react-router-dom';
+import './index.css';
+import cursor from './assets/mouse.png';
+import imgCursor from './assets/icons8-crocodile-64 (1).png';
 function App() {
+  const headerRef = useRef();
+  const [mouseX, setMouseX] = useState('');
+  const [mouseY, setMouseY] = useState('');
+  const [scrolllY, setScrollY] = useState('');
+
+  const moveCursor = (e) => {
+    setMouseX(e.clientX);
+    setMouseY(e.clientY);
+    if (headerRef.current) {
+      // Your code here
+
+      headerRef.current.style.transform = `translate3d(${mouseX}px, ${
+        mouseY + scrolllY
+      }px, 0)`;
+    }
+  };
+
+  window.addEventListener('scroll', () => {
+    setScrollY(window.scrollY);
+    if (headerRef.current) {
+      // Your code here
+      headerRef.current.style.transform = `translate3d(${mouseX}px, ${
+        mouseY + scrolllY
+      }px, 0)`;
+    }
+  });
+  window.addEventListener('mousemove', moveCursor);
   return (
-    <Router>
-			<Routes>
-				<Route path="/" element={<Landing />} />
+    <>
+      <div className="pointed" ref={headerRef}>
+        <img
+          width="64"
+          height="64"
+          src={imgCursor}
+          alt="external-crocodile-animal-flaticons-lineal-color-flat-icons-3"
+        />
+      </div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
         </Routes>
-        </Router>
+      </Router>
+    </>
   );
 }
 
